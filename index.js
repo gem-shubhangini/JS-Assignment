b97be76aadaf1d8f1d582b76743db1ed9da8f5ce
gem-shubhangini <110813757+gem-shubhangini@users.noreply.github.com>
2022-09-13 17:33:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,118 @@
-xhr.addEventListener("readystatechange", function() {
+var z = document.getElementById("create-new-task-block");
+ var savebtn=document.getElementById("save-button");
+ var cancelbtn=document.getElementById("cancel-button");
+ let form = document.getElementById("form");
+var taskName = document.getElementById("task-name");
+var statusbar = document.getElementById("task-status");
+var assign = document.getElementById("task-assign");
+var msg =form.getElementsByClassName("msg");
+var description = document.getElementById("task-description");
+var date =document.getElementById("task-duedate");
+function createTask(e) {
+    statusbar.value=e.parentElement.parentElement.id
+     z.style.display = "block";
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+    formValidation();
+    form.reset();
+    z.style.display = "none";
+});
+let formValidation = () => {
+    if (taskName.value === "") {
+       
+        console.log("failure");
+    } else {
+        console.log("successs");
+        addData();
+    }
+};
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var id = ev.currentTarget.id
+    var data = ev.dataTransfer.getData("text");
+    var card = document.getElementById(`${data}`)
+  
+   //var iod =card.data-id;
+ //var dataedit=JSON.stringify({ 
+//   "status":id,
+// });
+    // var xhr = new XMLHttpRequest();
+    // xhr.addEventListener("readystatechange", function() {
+    //   if(this.readyState === 4) {
+    //     console.log(this.responseText);
+    //   }
+    // }); 
+    // xhr.open("PUT", "myURL/iod");
+    // xhr.setRequestHeader("Content-Type", "application/json");   
+    // xhr.send(dataedit);
+    var body = card.querySelector('p')
+    body.innerHTML = `Status: <span class="test rounded-circle" id="${id}"></span>${id}`
+
+
+    ev.currentTarget.appendChild(document.getElementById(data));
+
+}
+
+
+cancelbtn.addEventListener('click',function closeCard() {
+     form.reset(); 
+    z.style.display = "none";
+ 
+})
+
+
+let data = [];
+
+
+let addData = () => {
+    
+    let Data = {
+        
+        taskName: taskName.value,
+        description: description.value,
+        DueDate:date.value,
+        status: statusbar.value,
+        Assignee:assign.value
+    };
+
+    data.push(Data);
+    // var datasend = JSON.stringify(data);
+      
+    //   var xhr = new XMLHttpRequest();
+      
+    //   xhr.addEventListener("readystatechange", function() {
+    //     if(this.readyState === 4) {
+    //       console.log(this.responseText);
+    //     }
+    //   });
+      
+    //   xhr.open("POST", "myURL");
+    //   xhr.setRequestHeader("Content-Type", "application/json");
+      
+    //   xhr.send(datasend);
+    localStorage.setItem('TaskData', JSON.stringify(data));
+    saveTask();
+}
+
+let saveTask = () => {
+//     var dataget = new FormData();
+
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function() {
 //   if(this.readyState === 4) {
 //     console.log(this.responseText);
 //   }
@@ -29,7 +143,7 @@ xhr.addEventListener("readystatechange", function() {
 var todo = document.getElementById(`${statusbar.value}`);
     if (data && data.length !== 0) {
         todo.innerHTML += `
-        <div class="card taskcard" id="${taskName.value.toLowerCase().split(" ").join("")}" data-id="1" draggable="true" ondragstart="drag(event)">
+        <div class="card taskcard" id="${taskName.value.toLowerCase().split(" ").join("")}"  draggable="true" ondragstart="drag(event)">
            <div class="taskFlex">
            <h5 class="card-title">${taskName.value}</h5>
            <span class="options">
